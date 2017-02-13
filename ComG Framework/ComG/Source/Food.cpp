1e32e118807020a3fd918e79fc50de76ff43669d
TheUnholyGod <kitsonlow99@gmail.com>
2017-02-13 12:03:58 +0800

    Uploaded Working Cope

--- ComG Framework/ComG/Source/Food.cpp
+++ ComG Framework/ComG/Source/Food.cpp
@@ -1,8 +1,8 @@
 #include "Food.h"
 
-Food::Food(std::string name, FOODTYPE type, float nutri, float life) :Item(name), TYPE(type), nutrition_(nutri), shelf_life_(life)
+Food::Food()
 {
-	
+
 }
 
 Food::~Food()
