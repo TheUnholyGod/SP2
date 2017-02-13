d8fd3f997e8907d2a36f47bf66d05bbb4ea2d872
hsy0090 <hsy0090.hsy@gmail.com>
2017-02-13 16:36:53 +0800

    Added player class

--- ComG Framework/ComG/Source/Player.h
+++ ComG Framework/ComG/Source/Player.h
@@ -1,15 +1,32 @@
-//#ifndef _PLAYER_H
-//#define _PLAYER_H
-//
-//class Player
-//{
-//private:
-//	Player();
-//	static Player* player;
-//
-//public:
-//	static Player* getplayer();
-//	~Player();
-//};
-//
-//#endif
+#ifndef _PLAYER_H
+#define _PLAYER_H
+
+#include "GameObject.h"
+#include <string>
+#include "Vector3.h"
+
+class Player : public GameObject
+{
+private:
+	Player(int);
+	static Player* player;
+	
+
+public:
+	static Player* getplayer(int);
+	const int gethealth();
+	const int gethunger();
+	const int getattack();
+	//void setposition(const Vector3& pos);
+	//const Vector3 getposition();
+	void receivedamage();
+	bool isDead();
+	~Player();
+
+protected:
+	//Vector3 position_;
+	float health_;
+	float hunger_;
+};
+
+#endif
