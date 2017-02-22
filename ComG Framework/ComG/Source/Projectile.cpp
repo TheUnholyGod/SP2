544c675c660339d9d91ebf41f5c40a65b40fdc5c
TheUnholyGod <kitsonlow99@gmail.com>
2017-02-22 16:00:44 +0800

    Changed Projectile from cam Forward to player Forward

--- ComG Framework/ComG/Source/Projectile.cpp
+++ ComG Framework/ComG/Source/Projectile.cpp
@@ -17,7 +17,7 @@ void Projectile::FireProjectile()
 	fired_ = true;
 	defaultpos = gameobjrenderer_->getPosition();
 	gameobjrenderer_->setPosition(Player::getplayer()->getWeapon()->getRenderer().getPosition());
-	gameobjrenderer_->setForward(Player::getplayer()->getWeapon()->getRenderer().getForward());
+	gameobjrenderer_->setForward(Player::getplayer()->getRenderer().getForward());
 }
 
 bool Projectile::hit(std::list<Building*>buildings, std::vector<Enemy*>enemies)
@@ -53,16 +53,12 @@ void Projectile::update(double dt, std::list<Building*>buildings, std::vector<En
 		gameobjrenderer_->setPosition(Player::getplayer()->getWeapon()->getRenderer().getPosition());
 		gameobjrenderer_->setUp(Player::getplayer()->getWeapon()->getRenderer().getForward().Cross(Player::getplayer()->getWeapon()->getRenderer().getRight()).Normalized());
 		gameobjrenderer_->setForward(Player::getplayer()->getWeapon()->getRenderer().getForward());
-		if (gameobjrenderer_->getForward().y < 0)
-		{
-			gameobjrenderer_->translate((0, 1, 0), gameobjrenderer_->getForward().y);
-		}
 	}
 
 	else
 	{
 		gameobjrenderer_->translate(gameobjrenderer_->getForward(), proj_speed_*dt);
-		if ((gameobjrenderer_->getPosition() - defaultpos).Length() > range_ || hit(buildings,enemies))
+		if ((gameobjrenderer_->getPosition() - defaultpos).Length() > range_ || hit(buildings,enemies) || gameobjrenderer_->getPosition().y < 0)
 		{
 			deletepls_ = true;
 		}
