38b988a33df8c93c61baa6056c26213c8661a5ed
TMP246\ValuedAcerCustomer <greatreaperzsnip3@gmail.com>
2017-02-27 16:23:44 +0800

    Trying to spawn Acid plant

--- ComG Framework/ComG/Source/Acrid_Plant.cpp
+++ ComG Framework/ComG/Source/Acrid_Plant.cpp
@@ -1,7 +1,8 @@
 #include "Acrid_Plant.h"
 
-Acrid_Plant::Acrid_Plant() : Enemy(12, "", "", "AcridPlant",  BOSS, "Base", 150, 15, 20)
+Acrid_Plant::Acrid_Plant() : Enemy(12, "OBJ//AcidPlant.obj", "Image//AcidPlant_Texture.tga", "AcridPlant",  BOSS, "Base", 150, 15, 20)
 {
+	allAABB[0]->setMinMax(gameobjrenderer_->getPosition());
 }
 
 Acrid_Plant::~Acrid_Plant()
