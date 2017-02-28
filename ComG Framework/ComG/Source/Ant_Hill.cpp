51f4900224785e61355632068ad445ab379fd581
TMP246\ValuedAcerCustomer <greatreaperzsnip3@gmail.com>
2017-02-28 15:51:38 +0800

    Updated Ant, not done yet

--- ComG Framework/ComG/Source/Ant_Hill.cpp
+++ ComG Framework/ComG/Source/Ant_Hill.cpp
@@ -1,13 +1,13 @@
 #include "Ant_Hill.h"
-
+#include "Randomizer.h"
 Ant_Hill::Ant_Hill() : Enemy(11,"OBJ//AntHill.obj" ,"Image//AntHill_Texture.tga", "AntHill", BOSS, "Base", 150, 20, 10)
 {
-	/*float ah_X = 
-		float ah_Y =
-		float ah_Z =*/
-	gameobjrenderer_->setPosition(Vector3(temp1, temp2, temp3));
+	float ah_X = Randomizer::generate_range(1, 100);
+	float ah_Z = Randomizer::generate_range(1, 100);
+	gameobjrenderer_->setPosition(Vector3(ah_X, 0, ah_Z));
 	allAABB[0]->setMinMax(gameobjrenderer_->getPosition());
 	allAABB[1]->setMinMax(gameobjrenderer_->getPosition());
+	hills.push_back(this);
 }
 
 Ant_Hill::~Ant_Hill()
--- ComG Framework/ComG/Source/Ant_Hill.h
+++ ComG Framework/ComG/Source/Ant_Hill.h
@@ -5,6 +5,7 @@
 class Ant_Hill : public Enemy
 {
 public:
+	static std::vector<Enemy*> hills;
 	Ant_Hill();
 	~Ant_Hill();
 	void Update(double dt) {}
